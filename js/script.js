18dec0d7fd205164bfb5256096180e6552ff59e9
unknown <duyndse150396@fpt.edu.vn>
2021-12-09 23:35:37 +0700

    ADD section scrolling

--- js/script.js
+++ js/script.js
@@ -10,3 +10,14 @@ function hideNavMobile() {
 
     document.getElementsByClassName("nav-mobile")[0].style.transform = "translateX(100%)";
 }
+
+function scrollToSection(section) {
+    const aboutme = document.getElementsByClassName('about-me')[0];
+
+
+    if (section === 'aboutme') {
+        aboutme.scrollIntoView({ behavior: "smooth", block: "center", inline: "nearest" });
+    }
+
+    hideNavMobile();
+}
