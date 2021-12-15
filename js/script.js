95c808e9d4f6c9a473332014c42d9be0e756dfab
unknown <duyndse150396@fpt.edu.vn>
2021-12-16 00:44:38 +0700

    captcha test

--- js/script.js
+++ js/script.js
@@ -1,9 +1,11 @@
 function showNavMobile() {
+    document.getElementsByTagName('body')[0].style.overflow = 'hidden';
     document.getElementsByClassName("nav-overlay")[0].style.display = "block", document.getElementsByClassName("nav-mobile")[0].style.transform = "translateX(0%)";
     for (var c = document.querySelectorAll(".nav-mobile ul li, .social-btn > a"), a = 0; a < c.length; a++) c[a].style.transform = "translate(0%, 0%) scale(1)"
 }
 
 function hideNavMobile() {
+    document.getElementsByTagName('body')[0].style.overflow = 'auto';
     document.getElementsByClassName("nav-overlay")[0].style.display = "none", document.getElementsByClassName("nav-mobile")[0].style.transform = "translateX(100%)";
     for (var c = document.querySelectorAll(".nav-mobile ul li, .social-btn > a"), a = 0; a < c.length; a++) c[a].style.transform = "translate(50%, 100%) scale(0)"
 }
